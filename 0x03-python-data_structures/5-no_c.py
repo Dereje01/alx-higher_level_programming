0842944ad3a60cc89304fd6b7d505d4ce886bdf6
dereje01 <derejemesfin3@gmail.com>
2022-09-25 11:45:01 -0700

    0x03-python-data_structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,10 +5,3 @@ def no_c(my_string):
         if char == 'c' or char == 'C':
             listofchars.remove(char)
     return("".join(listofchars))
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Stat
